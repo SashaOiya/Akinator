f9fd93bb3980369f875b57f79261aeb2134887ea
sasha_oiya_ssanina <141181890+SashaOiya@users.noreply.github.com>
2023-11-24 14:43:35 +0300

    Plus Dtor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+#define TX_SPEAK
+
+#ifdef TX_SPEAK
+    #include <TXLib.h>
+#endif
 
 typedef char* elem_t;
 #define SPECIFIER "%s"
@@ -12,10 +19,10 @@ typedef char* elem_t;
 #endif
 
 struct Knot {
-    elem_t value = 0;  // name //calloc
+    elem_t data  = 0;
     Knot *left   = 0;
     Knot *right  = 0;
-    int count = 0;  // chislo vhogdeniy
+    int count    = 0;  // chislo vhogdeniy
 };
 
 struct Tree_t {
@@ -25,14 +32,18 @@ struct Tree_t {
 
 enum Errors_t {
     ERR_NO   = 0,
-    ERR_READ = 1
-
+    ERR_READ = 1,
+    ERR_FILE = 2
 };
 
-void Print_Node  ( const struct Knot *tree_knot );
-void File_Write_Node  ( const struct Knot *tree_knot, FILE *tree );
-Errors_t File_Reader ( FILE * f, struct Knot **tree );
-void Graph_Dump  ( const struct Akin_t *tree );
+void     Tree_Text_Dump  ( const struct Knot *tree_knot );
+void     File_Write_Node ( const struct Knot *tree_knot, FILE *tree );
+Errors_t File_Reader     ( FILE * f, struct Knot **tree );
+void     Akinator        ( struct Knot **tree );
+void     Tree_Dtor       ( struct Knot *tree );
+void     Tree_Dump_Link  ( FILE *tree_dump, const struct Knot *tree );
+void     Tree_Graph_Dump ( const struct Knot *tree );
+void     Tree_Dump_Body  ( FILE *tree_dump, const struct Knot *tree );
 
 int main ()
 {
@@ -41,11 +52,20 @@ int main ()
 
     File_Reader ( tree_f, &tree.start );
 
-    File_Write_Node  ( tree.start, tree_f );
-
-    Print_Node ( tree.start );
+    Akinator ( &tree.start );
 
     fclose ( tree_f );
+
+    FILE *tree2_f = fopen ( "tree.txt", "w" );
+
+    File_Write_Node ( tree.start, tree2_f );
+    Tree_Graph_Dump ( tree.start );
+
+$   Tree_Text_Dump ( tree.start );
+
+    fclose ( tree_f );
+
+    Tree_Dtor ( tree.start );
 
     return 0;
 }
@@ -53,9 +73,13 @@ int main ()
 Errors_t File_Reader ( FILE * f, struct Knot **tree )
 {
     char word[100] = {};
-    fscanf ( f, "%s", &word ); //getline
+    //int size = 100;
 
-    if ( strcmp ( word, ")" ) == 0 ) {
+    fscanf ( f, "%s", &word );
+    //getdelim ( &word, &size, "\n", f );
+    //getline  ();
+
+    if ( strcmp ( word, ")" ) == 0 ) {    //switch
         fscanf ( f, "%s", &word );
     }
     if ( strcmp ( word, "(" ) == 0 ) {
@@ -67,7 +91,7 @@ $       fscanf ( f, "%s", &word );
 
         return ERR_NO;
     }
-$   (*tree)->value = strdup( word );
+$   (*tree)->data = strdup( word );
 
 $   File_Reader ( f, &((*tree)->left) );
     File_Reader ( f, &((*tree)->right) );
@@ -83,65 +107,139 @@ $   File_Reader ( f, &((*tree)->left) );
     return ERR_NO;
 }
 
-void Print_Node  ( const struct Knot *tree_knot )
+void Tree_Text_Dump ( const struct Knot *tree_knot )
 {
 
     if ( tree_knot == nullptr) {
-        printf ( " nil " );
+        printf     ( " nil " );
 
         return ;
     }
-    printf ( " ( " );
+    printf         ( " ( " );
+    printf         ( SPECIFIER, tree_knot->data );
 
-    printf ( SPECIFIER, tree_knot->value );
+    Tree_Text_Dump ( tree_knot->left );
+    Tree_Text_Dump ( tree_knot->right );
 
-    Print_Node ( tree_knot->left );
-    Print_Node ( tree_knot->right );
-
-    printf ( " ) " );
+    printf         ( " ) " );
 
 }
 
 void File_Write_Node  ( const struct Knot *tree_knot, FILE *tree_f )
 {
-    //FILE *tree = fopen ( "tree.txt", "w" );
-
     if ( tree_knot == nullptr) {
-        fprintf ( tree_f, " nil " );
+        fprintf     ( tree_f, "nil\n" );
 
         return ;
     }
-    fprintf ( tree_f, " ( \n" );
-
-    fprintf ( tree_f, SPECIFIER, tree_knot->value );
-    fprintf ( tree_f, "\n" );
+    fprintf         ( tree_f, "(\n" );
+    fprintf         ( tree_f, SPECIFIER, tree_knot->data );
+    fprintf         ( tree_f, "\n" );
 
     File_Write_Node ( tree_knot->left, tree_f );
     File_Write_Node ( tree_knot->right, tree_f );
 
-    fprintf ( tree_f,  " ) \n" );
+    fprintf         ( tree_f,  ")\n" );
 
 }
 
-/*void Akinator () {
-    while ( tree.data[ip].value != nullptr ) {
-        printf ( SPECIFIER, tree.data[ip].value );
-        printf ( "?\n" );
+void Akinator ( struct Knot **tree )
+{
+    printf ( SPECIFIER, (*tree)->data );
+    printf ( "?\n" );
 
-        char answer[10] = {};
-        scanf ( "%s", answer );
+    char answer[10] = {};
+    scanf ( "%s", answer );
 
-        if ( strcmp ( answer, "yes" ) == 0 ) {
-            ip = tree.data[ip].right->ip;
-            //printf ( SPECIFIER, tree.data[ip].value );
-        }
-        else if ( strcmp ( answer, "no" ) == 0 ) {
-            ip = tree.data[ip].left->ip;
-            //printf ( SPECIFIER, tree.data[ip].value );
-        }
+    if ( strcmp ( answer, "yes" ) == 0 ) {
+        printf ( "Stupid man, think of something more complicated! BUGAGA\n" );
+        txSpeak ( "Stupid man, think of something more complicated! BUGAGA\n" );
     }
-}   */
+    else if ( strcmp ( answer, "no" ) == 0 ) {
+        if ( (*tree)->left == nullptr ) {
+            printf ( "WHO ?\n" );
 
-//void File_Read_Node  ( const struct Knot *tree_knot, FILE *tree ) {
+            char word[100] = {};
+
+            (*tree)->left = (Knot *)calloc ( 1, sizeof ( Knot ) );
+$           scanf ( "%s", &word );
+            ((*tree)->left)->data = strdup( word );
+
+            printf ( "Now I know who it is. You can't beat me now \n" );
+
+            return ;
+        }
+        Akinator ( &(*tree)->left );
+    }
+}
+
+void Tree_Dtor ( struct Knot *tree )
+{
+    if ( tree != nullptr ) {
+        Tree_Dtor ( tree->left  );
+        Tree_Dtor ( tree->right );
+
+        free ( tree );
+    }
+
+    return ;
+}
+
+void Tree_Graph_Dump ( const struct Knot *tree )
+{
+    FILE *tree_dump = fopen ( "tree.dot", "w" );
+
+    fprintf ( tree_dump, "digraph G { \n"
+                         "rankdir = LR;\n"
+                         "node [shape = record];\n"
+                         " %o ", tree );
+
+    Tree_Dump_Link ( tree_dump, tree );
+
+    fprintf ( tree_dump, "[arrowsize = 0.0, weight = 10000, color = \"#FFFFFF\"];\n" );
+
+    Tree_Dump_Body ( tree_dump, tree );
+
+    fprintf ( tree_dump, "}\n" );
+
+    //system ( "del list.png" );
+    /*system ( "dot -T png tree.dot -o tree.png" );
+    system ( "tree.png" );  */
+
+    fclose ( tree_dump );
+}
+
+void Tree_Dump_Link ( FILE *tree_dump, const struct Knot *tree )
+{
+    if ( tree == nullptr) {
+
+        return ;
+    }
+    fprintf ( tree_dump, "-> %o", tree );
+
+    Tree_Dump_Link ( tree_dump, tree->left );
+    Tree_Dump_Link ( tree_dump, tree->right );
+}
+
+void Tree_Dump_Body ( FILE *tree_dump, const struct Knot *tree )
+{
+    if ( tree == nullptr) {
+
+        return ;
+    }
+    fprintf ( tree_dump , " %o [shape = Mrecord, style = filled, fillcolor = lightpink "
+                          " label = \"data: %s | right: %s | left: %s\"];\n ",tree, tree->data,
+                                                                              tree->right,
+                                                                              tree->left  );
+    if ( tree->left != nullptr ) {
+        fprintf ( tree_dump, "%o -> %o", tree, tree->left );
+    }
+    if ( tree->right != nullptr ) {
+        fprintf ( tree_dump, "\n %o -> %o \n", tree, tree->right );
+    }
+
+    Tree_Dump_Body ( tree_dump, tree->left );
+    Tree_Dump_Body ( tree_dump, tree->right );
+}
 
 
